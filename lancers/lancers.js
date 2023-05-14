ecdc61c30c7d9c335b3a0ee4c4df2e9a904ea62a
ekzemplaro <linux@hi-ho.ne.jp>
2023-05-14 17:03:53 +0900

    Commit on May/14/2023

--- lancers/lancers.js
+++ lancers/lancers.js
@@ -4,23 +4,118 @@
 //					May/14/2023
 //
 // -----------------------------------------------------------------------
-var app = angular.module('myApp', [])
-const url_in = "./convert.py"
-// const url_in = "./projects.json"
+'use strict'
+// -----------------------------------------------------------------------
+window.onload = ()=>
+{
+	document.querySelector("#outarea_aa").innerText = "*** lancers.js *** start ***"
 
-app.controller('MainController', ['$scope', '$timeout', '$http', function($scope, $timeout, $http){
-    $timeout(function() {
-$http.get(url_in)
-	.then(function(res) {
-	console.log ('読み込み成功')
-	console.log (res.data)
-	$scope.items = res.data
-    })
-	.catch (function(err) {
-		alert('読み込み失敗')
-    })
+	const file_json = "./projects.json"
+	const place = ".contents"
+	read_fetch_table_proc(file_json,place)
+
+	document.querySelector("#outarea_hh").innerText = "*** lancers.js *** end ***"
+
+}
+
+// -----------------------------------------------------------------------
+function read_fetch_table_proc(url,place)
+{
+	fetch(url).then((response) => {
+		if(!response.ok) {
+			console.log('Read error!')
+			throw new Error('error')
+		} 
+		console.log('Read ok!')
+		return response.text()
+	}).then((data)  => {
+//		console.log(data)
+
+		const data_aa = JSON.parse(data)
+		const array_aa = sort_key_proc(data_aa)
+
+		show_table_proc(place,array_aa)
 })
 
-}])
+}
+
+// -----------------------------------------------------------------------
+function sort_key_proc (dict_aa)
+{
+	var array = new Array()
+
+	for(var it in dict_aa)
+		{
+		array.push({'key':String (it), 'value':dict_aa[it]})
+		}
+
+	array.sort(compare_by_key_proc)
+
+	return array
+}
+
+// ---------------------------------------------------------------
+function compare_by_key_proc(left,right)
+{
+	var aa = left.key
+	var bb = right.key
+
+	var rvalue = 0
+
+	if (aa > bb)
+		{
+		rvalue = -1
+		}
+	else if (aa < bb)
+		{
+		rvalue = 1
+		}
+
+	return	rvalue
+}
+
+// -----------------------------------------------------------------------
+// [4]:
+function show_table_proc(place,array_aa)
+{
+	var str_out = ""
+	str_out += "<table>"
+	str_out += "<tr>"
+	str_out += "<th>no</th>"
+	str_out += "<th>url</th>"
+	str_out += "<th>start</th>"
+	str_out += "<th>end</th>"
+	str_out += "<th>fee</th>"
+	str_out += "<th>client</th>"
+	str_out += "<th>title</th>"
+	str_out += "</tr>"
+
+	array_aa.forEach(function (aax)
+		{
+		str_out += record_proc(aax.key,aax.value)
+		})
+
+	str_out += "</table>"
+
+	document.querySelector(place).innerHTML = str_out
+}
+
+// -----------------------------------------------------------------------
+function record_proc(key,unit_aa)
+{
+	var str_out = "<tr>"
+	str_out += "<td>" + key + "</td>"
+	str_out += '<td><a href="https://www.lancers.jp/work/detail/'
+	str_out += unit_aa.url + '">'
+	str_out += unit_aa.url + "</a></td>"
+	str_out += "<td>" + unit_aa.start + "</td>"
+	str_out += "<td>" + unit_aa.end + "</td>"
+	str_out += "<td>" + unit_aa.fee + "</td>"
+	str_out += "<td>" + unit_aa.client + "</td>"
+	str_out += "<td>" + unit_aa.title + "</td>"
+	str_out += "</tr>"
+
+	return str_out
+}
 
 // -----------------------------------------------------------------------
